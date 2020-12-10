edd341805c41c248bbf06037d66b05fe9973485c
Owen Synge <osynge@googlemail.com>
2020-12-10 21:51:07 +0100

    we only need a str

--- src/parse_glob.rs
+++ src/parse_glob.rs
@@ -1,6 +1,6 @@
 use glob::glob;
 
-fn load_glob(glob_str: &String) -> Result<Vec<xunit_repo_interface::File>, crate::error::LocalErr> {
+fn load_glob(glob_str: &str) -> Result<Vec<xunit_repo_interface::File>, crate::error::LocalErr> {
     let mut output = vec![];
     for path in glob(glob_str)? {
         let ding = path?;
