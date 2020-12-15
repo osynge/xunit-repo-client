d9f1c02b0f6b5eb20460df08876528b9c2d09599
Owen Synge <osynge@googlemail.com>
2020-12-15 23:21:07 +0100

    Fix defaulting and add tests

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Config {
     pub configfile: Option<String>,
     pub loglevel: Option<i8>,
@@ -11,7 +11,6 @@ pub struct Config {
     pub server_host: Option<String>,
     pub server_port: Option<u32>,
 }
-
 
 impl Config {
     //set the method's context
@@ -30,38 +29,141 @@ impl Config {
         }
     }
     pub fn default(&self, src: &Config) -> Config {
-        let mut out = self.clone();
-        if out.configfile.is_none() {
-            out.configfile = src.configfile.clone();
+        let configfile = match self.configfile.as_ref().or_else(|| src.configfile.as_ref()) {
+            Some(p) => Some(p.clone()),
+            None => None,
         };
-        if out.loglevel.is_none() {
-            out.loglevel = src.loglevel;
+        let loglevel = self.loglevel.or_else(|| src.loglevel);
+        let xunit_local_globs = match self
+            .xunit_local_globs
+            .as_ref()
+            .or_else(|| src.xunit_local_globs.as_ref())
+        {
+            Some(p) => Some(p.clone()),
+            None => None,
         };
-        if out.xunit_local_globs.is_none() {
-            out.xunit_local_globs = src.xunit_local_globs.clone();
+        let enviroment_sk = match self
+            .enviroment_sk
+            .as_ref()
+            .or_else(|| src.enviroment_sk.as_ref())
+        {
+            Some(p) => Some(p.clone()),
+            None => None,
         };
-        if out.enviroment_sk.is_none() {
-            out.enviroment_sk = src.enviroment_sk.clone();
+        let enviroment_keys = match self
+            .enviroment_keys
+            .as_ref()
+            .or_else(|| src.enviroment_keys.as_ref())
+        {
+            Some(p) => Some(p.clone()),
+            None => None,
         };
-        if out.enviroment_keys.is_none() {
-            out.enviroment_keys = src.enviroment_keys.clone();
+        let project_sk = match self.project_sk.as_ref().or_else(|| src.project_sk.as_ref()) {
+            Some(p) => Some(p.clone()),
+            None => None,
         };
-        if out.project_sk.is_none() {
-            out.project_sk = src.project_sk.clone();
+        let project_identiifier = match self
+            .project_identiifier
+            .as_ref()
+            .or_else(|| src.project_identiifier.as_ref())
+        {
+            Some(p) => Some(p.clone()),
+            None => None,
         };
-        if out.project_identiifier.is_none() {
-            out.project_identiifier = src.project_identiifier.clone();
-        };
-        if out.project_human_name.is_none() {
-            out.project_human_name = src.project_human_name.clone();
-        };
-        if out.server_host.is_none() {
-            out.server_host = src.server_host.clone();
-        };
-        if out.server_port.is_none() {
-            out.server_port = src.server_port;
-        };
-        out
 
+        let project_human_name = match self
+            .project_human_name
+            .as_ref()
+            .or_else(|| src.project_human_name.as_ref())
+        {
+            Some(p) => Some(p.clone()),
+            None => None,
+        };
+        let server_host = match self
+            .server_host
+            .as_ref()
+            .or_else(|| src.server_host.as_ref())
+        {
+            Some(p) => Some(p.clone()),
+            None => None,
+        };
+        let server_port = self.server_port.or_else(|| src.server_port);
+        Config {
+            configfile,
+            loglevel,
+            xunit_local_globs,
+            enviroment_sk,
+            enviroment_keys,
+            project_sk,
+            project_identiifier,
+            project_human_name,
+            server_host,
+            server_port,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    fn gen_config_with_data_1() -> Config {
+        Config {
+            configfile: Some(String::from("configfile")),
+            loglevel: Some(1),
+            xunit_local_globs: Some(vec![String::from("xunit_local_globs")]),
+            enviroment_sk: Some(String::from("enviroment_sk")),
+            enviroment_keys: Some(vec![String::from("enviroment_keys")]),
+            project_sk: Some(String::from("project_sk")),
+            project_identiifier: Some(String::from("project_identiifier")),
+            project_human_name: Some(String::from("project_human_name")),
+            server_host: Some(String::from("server_host")),
+            server_port: Some(8080),
+        }
+    }
+    fn gen_config_with_data_2() -> Config {
+        Config {
+            configfile: Some(String::from("2")),
+            loglevel: Some(1),
+            xunit_local_globs: Some(vec![String::from("2")]),
+            enviroment_sk: Some(String::from("2")),
+            enviroment_keys: Some(vec![String::from("2")]),
+            project_sk: Some(String::from("2")),
+            project_identiifier: Some(String::from("2")),
+            project_human_name: Some(String::from("2")),
+            server_host: Some(String::from("2")),
+            server_port: Some(2),
+        }
+    }
+
+    #[test]
+    fn gets_default_with_none() {
+        let a = gen_config_with_data_1();
+        let b = Config::new();
+        let c = b.default(&a);
+        assert_eq!(c, a);
+    }
+
+    #[test]
+    fn gets_none_with_none() {
+        let a = Config::new();
+        let b = Config::new();
+        let c = b.default(&a);
+        assert_eq!(c, a);
+    }
+
+    #[test]
+    fn gets_original_with_none() {
+        let a = gen_config_with_data_1();
+        let b = Config::new();
+        let c = a.default(&b);
+        assert_eq!(c, a);
+    }
+
+    #[test]
+    fn gets_original_with_some() {
+        let a = gen_config_with_data_1();
+        let b = gen_config_with_data_2();
+        let c = a.default(&b);
+        assert_eq!(c, a);
     }
 }
