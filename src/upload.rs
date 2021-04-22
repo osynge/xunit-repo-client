3ff7c463c327204d5388ad960d745d491535f651
Owen Synge <osynge@googlemail.com>
2021-04-22 14:23:05 +0200

    handle long requests

--- src/upload.rs
+++ src/upload.rs
@@ -1,14 +1,23 @@
+use std::time::Duration;
+
 pub fn upload(
     url: &String,
     payload: &xunit_repo_interface::Upload,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let url = format!("{url}/upload", url = url);
-    let client = reqwest::blocking::Client::new();
+    let client = reqwest::blocking::Client::builder()
+        .timeout(Duration::from_secs(600))
+        .build()?;
     let resp = client
         .post(&url)
         .json::<xunit_repo_interface::Upload>(&payload)
         .send();
     println!("{:#?}", resp);
-    println!("{:#?}", resp.unwrap().text());
+    let text = match resp {
+        Ok(p) => p.text(),
+        Err(_) => Ok(String::from("")),
+    };
+    println!("resp.text={:#?}", text);
+
     Ok(())
 }
