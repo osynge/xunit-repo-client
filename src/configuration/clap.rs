effd8efce32c20bc0651e7a86dce12a1beaedc67
Owen Synge <osynge@googlemail.com>
2023-02-07 23:04:22 +0100

    migrate to clap v4

--- src/configuration/clap.rs
+++ src/configuration/clap.rs
@@ -1,10 +1,9 @@
-use clap::App;
-use clap::Arg;
 use clap::{crate_name, crate_version};
+use clap::{Arg, ArgAction, Command};
 
 use std::convert::TryFrom;
 
-fn log_setup(verbose_occurrences: u64, quiet_occurrences: u64) -> Option<i8> {
+fn log_setup(verbose_occurrences: u8, quiet_occurrences: u8) -> Option<i8> {
     if (0, 0) == (verbose_occurrences, quiet_occurrences) {
         return None;
     };
@@ -20,116 +19,112 @@ fn log_setup(verbose_occurrences: u64, quiet_occurrences: u64) -> Option<i8> {
 }
 
 pub fn cli_clap() -> crate::configuration::Config {
-    let application = App::new(crate_name!())
+    let application = Command::new(crate_name!())
         .about("Parses an input file to do awesome things")
         .version(crate_version!())
         .author("Owen Synge <osynge@googlemail.com>")
         .arg(
-            Arg::with_name("verbose")
+            Arg::new("verbose")
                 .help("Increase log output.")
-                .short("v")
-                .multiple(true)
+                .short('v')
+                .action(ArgAction::Count)
                 .long("verbose"),
         )
         .arg(
-            Arg::with_name("quiet")
+            Arg::new("quiet")
                 .help("Decrease log output.")
-                .short("q")
-                .multiple(true)
+                .short('q')
+                .action(ArgAction::Count)
                 .long("quiet"),
         )
         .arg(
-            Arg::with_name("environment_variable")
-                .short("e")
+            Arg::new("environment_variable")
+                .short('e')
                 .long("env")
                 .value_name("ENVIROMENT_VARIABLE")
                 .help("Which environment variables to process")
-                .multiple(true)
-                .takes_value(true),
+                .action(ArgAction::Append),
         )
         .arg(
-            Arg::with_name("xunit")
+            Arg::new("xunit")
                 .long("xunit")
                 .value_name("XUNIT_FILES")
                 .help("glob of xunit files")
-                .multiple(true)
-                .takes_value(true),
+                .action(ArgAction::Append),
         )
         .arg(
-            Arg::with_name("config")
-                .short("c")
+            Arg::new("config")
+                .short('c')
                 .long("config")
                 .value_name("FILE")
                 .help("Sets a custom config file")
-                .takes_value(true),
+                .action(ArgAction::Append),
         )
         .arg(
-            Arg::with_name("environment-key")
+            Arg::new("environment-key")
                 .long("environment-id")
                 .value_name("ENV_ID")
                 .help("directory storing jobs shell scripts.")
-                .multiple(true)
-                .takes_value(true),
+                .action(ArgAction::Append),
         )
         .arg(
-            Arg::with_name("project-identifier")
+            Arg::new("project-identifier")
                 .long("project-identifier")
                 .value_name("PROJECT_IDENTIFIER")
                 .help("An identifier for the project such as VCS checkout url")
-                .takes_value(true),
+                .action(ArgAction::Append),
         )
         .arg(
-            Arg::with_name("project-name")
+            Arg::new("project-name")
                 .long("project-name")
                 .value_name("PROJECT_NAME")
                 .help("A name for the project.")
-                .takes_value(true),
+                .action(ArgAction::Append),
         )
         .arg(
-            Arg::with_name("run-identifier")
+            Arg::new("run-identifier")
                 .long("run-identifier")
                 .value_name("RUN_IDENTIFIER")
                 .help("An identifier for the run such as jenkins build number")
-                .takes_value(true),
+                .action(ArgAction::Append),
         )
         .arg(
-            Arg::with_name("run-key")
+            Arg::new("run-key")
                 .long("run-key")
                 .value_name("RUN_KEY")
                 .help("The key for the run")
-                .takes_value(true),
+                .action(ArgAction::Append),
         )
         .arg(
-            Arg::with_name("service-url")
+            Arg::new("service-url")
                 .long("url")
                 .value_name("SERVICE_URL")
                 .help("Sets the service url to upload the xunit to.")
-                .takes_value(true),
+                .action(ArgAction::Append),
         )
         .arg(
-            Arg::with_name("globs")
+            Arg::new("globs")
                 .help("Sets the input files to use, wild cards are allowed.")
                 .value_name("GLOB")
-                .multiple(true),
+                .action(ArgAction::Append),
         );
 
     let matches = application.get_matches();
-    let loglevel = log_setup(
-        matches.occurrences_of("verbose"),
-        matches.occurrences_of("quiet"),
-    );
+    let loglevel = log_setup(matches.get_count("verbose"), matches.get_count("quiet"));
     let xunit_local_globs = matches
-        .values_of("xunit")
+        .get_many::<String>("xunit")
         .map(|itr| itr.into_iter().map(String::from).collect());
     let environment_keys = matches
-        .values_of("environment_variable")
+        .get_many::<String>("environment_variable")
         .map(|itr| itr.into_iter().map(String::from).collect());
-    let config_file = matches.value_of("config").map(String::from);
-    let environment_sk = matches.value_of("environment-key").map(String::from);
-    let project_identifier = matches.value_of("project-identifier").map(String::from);
-    let project_human_name = matches.value_of("project-name").map(String::from);
-    let run_identifier = matches.value_of("run-identifier").map(String::from);
-    let service_url = matches.value_of("service-url").map(String::from);
+
+    let config_file = matches.get_one::<String>("config").cloned();
+    let environment_sk = matches.get_one::<String>("environment-key").cloned();
+    let project_identifier = matches.get_one::<String>("project-identifier").cloned();
+    let project_human_name = matches.get_one::<String>("project-name").cloned();
+    let run_identifier = matches.get_one::<String>("run-identifier").cloned();
+    let service_url = matches.get_one::<String>("service-url").cloned();
+
     crate::configuration::Config {
         config_file,
         loglevel,
